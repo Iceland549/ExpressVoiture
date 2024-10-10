0948c8233c5977ec1cdad5c96278ba54953ca846
iceland549 <chaudenisbreiz@yahoo.fr>
2024-10-10 12:58:29 +0200

    Add to App DbContext Utilisateur, DbSet Voiture and OnModelCreating

--- ExpressVoiture/Data/ApplicationDbContext.cs
+++ ExpressVoiture/Data/ApplicationDbContext.cs
@@ -3,11 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ExpressVoiture.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Utilisateur>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Voiture> Voitures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuration des propriétés de la classe Voiture
+            modelBuilder.Entity<Voiture>()
+                .Property(v => v.CoutsReparation)
+                .HasColumnType("decimal(18, 2)"); // 18 chiffres au total, 2 chiffres après la virgule
+
+            modelBuilder.Entity<Voiture>()
+                .Property(v => v.PrixAchat)
+                .HasColumnType("decimal(18, 2)"); // 18 chiffres au total, 2 chiffres après la virgule
+
+            modelBuilder.Entity<Voiture>()
+                .Property(v => v.PrixVente)
+                .HasColumnType("decimal(18, 2)"); // 18 chiffres au total, 2 chiffres après la virgule
+        }
+
+
     }
 }
